67bb9069d8ad648f14d5cc58450eec9d908fdddf
hananebz <37369006+hananebz@users.noreply.github.com>
2018-10-07 11:10:25 +0100

    Update HomeScreen.js

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,114 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
-import Slideshow from 'react-native-slideshow';
-import HeaderComponent from './Header';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Dimensions
+} from 'react-native';
 import { Container, Content, Card, Button } from 'native-base';
-import { DrawerNavigator, StackNavigator, DrawerItems, SafeAreaView } from 'react-navigation'
+import Slideshow from 'react-native-slideshow';
+import Swiper from 'react-native-swiper';
+import HeaderComponent from './Header';
+const { width, height } = Dimensions.get('window');
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    width: 410,
+     height: 280, 
+    // marginTop:165
+    
+  },
 
-export default class HomeScreen extends Component{
+
+  text: {
+    color: '#008000',
+    fontSize: 28,
+    fontWeight: 'bold',
+    borderColor : '#008000',
+    borderWidth : 2,
+    padding : 20,
+    paddingLeft : 40,
+    paddingRight : 40,
+    backgroundColor: 'rgba(255,255,255,.1)',
+    marginTop : 90,
+  },
+  text1 : {
+    color: '#008000',
+    fontSize: 28,
+    fontWeight: 'bold',
+    borderColor : '#008000',
+    borderWidth : 2,
+    padding : 20,
+    paddingLeft : 125,
+    paddingRight : 40,
+    backgroundColor: 'rgba(255,255,255,.1)',
+    marginTop : 90,
+
+  },
+  overlayC : {
+    flex : 1,
+    backgroundColor: 'rgba(47,163,218,.2)',
+   
+  }
+})
+
+export default class HomeScreen extends Component {
   openDrawer() {
     this.props.navigation.navigate('DrawerOpen');
 }
-static navigationOptions = {
-  drawerIcon: (
-      <Image source={require('../images/home.png')}
-          style={{ height: 35, width: 35}} />
-  )
-}
-	  constructor(props) {
-    super(props);
- 
-    this.state = {
-      position: 1,
-      interval: null,
-      dataSource: [
-        {
-          title: 'Les animaux de la ferme',
-          //caption: 'Caption 1',
-          url:require('../images/fermeAn.jpg'),
-         
-
-        }, {
-          title: 'Les animaux sauvages',
-          //caption: 'Caption 2',
-          url:require('../images/AniSau.jpg'),
-        }, {
-          title: 'Les animaux aquatiques',
-          //caption: 'Caption 3',
-          url: require('../images/AquaAni.jpg'),
-        },
-        {
-          title: 'Les oiseaux',
-          //caption: 'Caption 3',
-          url: require('../images/bird.jpg'),
-        },
-        
-      ],
-    };
-  }
- 
-  componentWillMount() {
-    this.setState({
-      interval: setInterval(() => {
-        this.setState({
-          position: this.state.position === this.state.dataSource.length ? 0 : this.state.position + 1
-        });
-      }, 2000)
-    });
-  }
- 
-  componentWillUnmount() {
-    clearInterval(this.state.interval);
-  }
-  render() {
-     return (
-       
+  render(){
+    return (
       <Container>
-      <HeaderComponent openDrawer={() => this.openDrawer()}
-              />
-      <Content>   
-      <View style={styles.row}>
-                       
-                    </View>     
-    <Slideshow 
-        dataSource={this.state.dataSource} style={styles.Slide}
-        position={this.state.position}
-        onPositionChanged={position => this.setState({ position })} />
-         </Content> 
-    </Container>    
+      <HeaderComponent openDrawer={() => this.openDrawer()}/>
+      <Swiper style={styles.wrapper} showsButtons={true}  nextButton= { <Image source={require('../images/next.png')} style={{ height: 35, width: 35}}/>} prevButton= { <Image source={require('../images/prev.png')} style={{ height: 37, width: 37}}/>}>
+       
+       <ImageBackground  source={require('../images/back.jpg')} style={{width: width, height: height}}> 
+        <View style={styles.overlayC}>
+        <Text style={styles.text}  onPress={() => this.props.navigation.navigate('Jouer')}>Les Animaux de la ferme</Text>
+        <Image source={require('../images/fermeAn.jpg')}style={styles.slide1}/>
+        </View>
+        </ImageBackground >
+
+        <ImageBackground  source={require('../images/back2.png')} style={{width: width, height: height}}> 
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Les animaux sauvages</Text>
+          <Image source={require('../images/AniSau.jpg')}/>
+        </View>
+        </ImageBackground >
+
+        <ImageBackground  source={require('../images/back3.jpg')} style={{width: width, height: height}}> 
+        <View style={styles.slide3}>
+          <Text style={styles.text1}>Les oiseaux</Text>
+          <Image source={require('../images/bird.jpg')} onPress={() => this.props.navigation.navigate('Accueil')} style={styles.slide1}/>
+        </View>
+        </ImageBackground >
+
+        <ImageBackground  source={require('../images/back4.jpg')} style={{width: width, height: height}}> 
+        <View style={styles.slide4}>
+          <Text style={styles.text}>Les animaux aquatiques</Text>
+          <Image source={require('../images/AquaAni.jpg')} onPress={() => this.props.navigation.navigate('Accueil')} style={styles.slide1}/>
+        </View>
+        </ImageBackground >
+      </Swiper>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 70,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  row: {
-   
-    color: '#333333',
-    marginBottom: 200,
-  },
- 
-});
